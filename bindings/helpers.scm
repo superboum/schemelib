8ab00998ac6ee385e134dd374800b99b24fb58ee
Quentin <quentin@deuxfleurs.fr>
2020-12-14 18:08:58 +0100

    sodium + helpers

---
+++ bindings/helpers.scm
@@ -0,0 +1,52 @@
+(library (schemelib bindings helpers)
+  (export
+    char*->string
+    string->char*
+    alloc alloc2
+    check-err check-perr)
+  (import 
+    (schemelib bindings libc errno)
+    (chezscheme))
+
+  ; Convert a char* to a Scheme string
+  ; Extracted from this paper:
+  ; http://scheme2011.ucombinator.org/papers/Keep2011.pdf
+  (define (char*->string fptr maxsize)
+    (let f ([i 0])
+      (let ([c (ftype-ref char () fptr i)])
+        (if (or (char=? c #\nul) (and (> maxsize 0) (>= i maxsize)))
+            (make-string i)
+            (let ([str (f (fx+ i 1))])
+              (string-set! str i c) str)))))
+
+  (define (string->char* src dest destmax)
+    (let ([strlen (string-length src)])
+      (let f ([i 0])
+        (cond
+          ((or (>= i strlen) (>= i destmax)) dest)
+          (#t 
+            (ftype-set! char () dest i (string-ref src i))
+            (f (fx+ i 1)))))))
+
+  (define (alloc size fx)
+    (let* ([v (foreign-alloc size)]
+           [r (fx v)])
+      (foreign-free v)
+      r))
+
+  (define (alloc2 l fx)
+    (let* ([v (map (lambda (size) (foreign-alloc size)) l)]
+           [r (apply fx v)])
+      (for-each (lambda (alloc) (foreign-free alloc)) v)
+      r))
+
+  (define (check-err out)
+    (assert (= 0 out)))
+
+  (define (check-perr ret msg)
+    (cond
+      ((< ret 0)
+        (perror msg)
+        (raise msg))
+      (#t ret)))
+)
--- bindings/sodium.scm
+++ bindings/sodium.scm
@@ -1,53 +1,73 @@
-(define sodium-path "libsodium.so.23")
-(if 
-	(file-exists? sodium-path)
-  (load-shared-object sodium-path)
-	(printf "[sodium] ~a not found, not loading lib" sodium-path))
-
-(define (sodium_init)
-  ((foreign-procedure "sodium_init" () int)))
-
-(define (randombytes_buf buf size)
-  ((foreign-procedure "randombytes_buf" (u8* size_t) void) buf size))
-
-(define (sodium_increment n nlen)
-  ((foreign-procedure "sodium_increment" (u8* size_t) void) n nlen))
-
-(define crypto_secretbox_KEYBYTES 32)
-(define crypto_secretbox_MACBYTES 16)
-(define crypto_secretbox_NONCEBYTES 24)
-
-(define crypto_box_PUBLICKEYBYTES 32)
-(define crypto_box_SECRETKEYBYTES 32)
-(define crypto_box_MACBYTES 16)
-(define crypto_box_SEALBYTES 
-  (+ 
+(library (schemelib bindings sodium)
+  (export
+    ; constants
+    crypto_secretbox_KEYBYTES
+    crypto_secretbox_MACBYTES
+    crypto_secretbox_NONCEBYTES
     crypto_box_PUBLICKEYBYTES
-    crypto_box_MACBYTES))
+    crypto_box_SECRETKEYBYTES
+    crypto_box_MACBYTES
+    crypto_box_SEALBYTES
+    ; procedures
+    sodium_init
+    randombytes_buf
+    sodium_increment
+    crypto_box_keypair
+    crypto_box_seal
+    crypto_box_seal_open
+    crypto_secretbox_easy
+    crypto_secretbox_open_easy)
+  (import (chezscheme))
 
-(define (crypto_box_keypair pk sk)
-  ((foreign-procedure "crypto_box_keypair" (u8* u8*) int) pk sk))
+  ;--- constants
+  (define crypto_secretbox_KEYBYTES 32)
+  (define crypto_secretbox_MACBYTES 16)
+  (define crypto_secretbox_NONCEBYTES 24)
 
-(define (crypto_box_seal c m mlen pk)
-  ((foreign-procedure 
-     "crypto_box_seal" 
-     (u8* u8* unsigned-64 u8*) 
-     int) c m mlen pk))
+  (define crypto_box_PUBLICKEYBYTES 32)
+  (define crypto_box_SECRETKEYBYTES 32)
+  (define crypto_box_MACBYTES 16)
+  (define crypto_box_SEALBYTES 
+    (+ 
+      crypto_box_PUBLICKEYBYTES
+      crypto_box_MACBYTES))
 
-(define (crypto_box_seal_open m c clen pk sk)
-  ((foreign-procedure
-    "crypto_box_seal_open"
-    (u8* u8* unsigned-64 u8* u8*) 
-    int) m c clen pk sk))
+  ;--- procedures
+  (define (sodium_init)
+    ((foreign-procedure "sodium_init" () int)))
 
-(define (crypto_secretbox_easy c m mlen n k)
-  ((foreign-procedure
-    "crypto_secretbox_easy"
-    (u8* u8* unsigned-64 u8* u8*)
-    int) c m mlen n k))
+  (define (randombytes_buf buf size)
+    ((foreign-procedure "randombytes_buf" (u8* size_t) void) buf size))
 
-(define (crypto_secretbox_open_easy m c clen n k)
-  ((foreign-procedure
-    "crypto_secretbox_open_easy"
-    (u8* u8* unsigned-long u8* u8*)
-    int) m c clen n k))
+  (define (sodium_increment n nlen)
+    ((foreign-procedure "sodium_increment" (u8* size_t) void) n nlen))
+
+  (define (crypto_box_keypair pk sk)
+    ((foreign-procedure "crypto_box_keypair" (u8* u8*) int) pk sk))
+
+  (define (crypto_box_seal c m mlen pk)
+    ((foreign-procedure 
+      "crypto_box_seal" 
+      (u8* u8* unsigned-64 u8*) 
+      int) c m mlen pk))
+
+  (define (crypto_box_seal_open m c clen pk sk)
+    ((foreign-procedure
+      "crypto_box_seal_open"
+      (u8* u8* unsigned-64 u8* u8*) 
+      int) m c clen pk sk))
+
+  (define (crypto_secretbox_easy c m mlen n k)
+    ((foreign-procedure
+      "crypto_secretbox_easy"
+      (u8* u8* unsigned-64 u8* u8*)
+      int) c m mlen n k))
+
+  (define (crypto_secretbox_open_easy m c clen n k)
+    ((foreign-procedure
+      "crypto_secretbox_open_easy"
+      (u8* u8* unsigned-long u8* u8*)
+      int) m c clen n k))
+
+  (load-shared-object "libsodium.so.23")
+)
