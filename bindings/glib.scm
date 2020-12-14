de73a3e3c09a9e12646895fa30401ea028f68e9e
Quentin Dufour <quentin@dufour.io>
2020-12-14 10:02:28 +0000

    Add read int

--- bindings/glib.scm
+++ bindings/glib.scm
@@ -1,4 +1,4 @@
-(load-shared-object "libglib-2.0.so")
+(load-shared-object "libglib-2.0.so.0")
 
 (define (g-main-loop-new context is-running)
   ((foreign-procedure "g_main_loop_new" (void* boolean) void*) context is-running))
